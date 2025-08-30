18be1e5bfb5d9f0bb5090701c293c935efd405d9
JBalladaresM2 <jballadaresm2@unemi.edu.ec>
2025-08-30 16:17:04 -0500

    incremento

---
+++ javascripts/ejercicio_19.js
@@ -0,0 +1,23 @@
+// N personas con clasificación de edades //
+
+const cantidad_edades = parseInt(prompt("Ingresa cantidad de edades a clasificar:"))
+
+for (let i = 0; i < cantidad_edades; i++) {
+    const edad = parseInt(prompt(`Edad #${i+1} - Ingresa una edad:`)) || 0
+
+    console.log(`* La edad #${i+1} es:`)
+
+    if (edad >= 65) {
+        console.log("- Adulto mayor")
+    } else if (edad >= 36) {
+        console.log("- Adulto")
+    } else if (edad >= 18) {
+        console.log("- Joven")
+    } else if (edad >= 14) {
+        console.log("- Adolescente")
+    } else if (edad >= 4) {
+        console.log("- Niño")
+    } else if (edad >= 1) {
+        console.log("- Infante")
+    }
+}
