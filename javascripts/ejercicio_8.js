a2f9ec0804d28234ecbe0fbea82f1700b770b0e7
st3benm <stevenmontalvan@gmail.com>
2025-09-07 15:36:06 -0500

    XD

--- javascripts/ejercicio_8.js
+++ javascripts/ejercicio_8.js
@@ -1,8 +1,9 @@
-let peso;
-const valor=30
+function ejecutar_8(valor){
+let peso;                           //INGRESAMOS LAS VARIABLES Y ABRIMOS UN PROMPT DE DONDE SE ENVIARAN LOS PAQUETES
 let destino;
-destino=prompt("Ingrese el destino del paquete (Nacional o internacional)")
+destino=prompt("Ingrese el destino del paquete (Nacional o internacional)")     //EL VALOR YA ESTA PUESTO Y SOLAMENTE ABRIA QUE INGRESAR EL PESO 
 peso=parseInt(prompt("Ingrese el peso del paquete en kg"))
-alert(`Su paquete se dirige a ${destino} con un peso de ${peso}kg ,  el precio po kg es de ${valor}$ y el costo es de ${valor*peso} dolares `)
-
-
+total=valor*peso                                                                              //CALCULAMOS Y PRESENTAMOS
+console.log(`Su paquete se dirige a ${destino} con un peso de ${peso}kg ,  el precio po kg es de ${valor}$ y el costo es de ${total} dolares `)
+}
+ejecutar_8(30)
