51c798a5377ff355007457a06ef1edaf3816b904
st3benm <stevenmontalvan@gmail.com>
2025-09-09 08:47:24 -0500

    correcion

--- javascripts/ejercicio_2.js
+++ javascripts/ejercicio_2.js
@@ -10,5 +10,5 @@ if(cantidad>=1000){
 } else if (cantidad < 1000){
     costo=0.90
       total=cantidad*costo
-    console.log(`Total a pagar ${total} `)     //POR ULTIMO HACEMOS LA FORMULA PARA SABER EL VALOR EXACTO
+    console.log(`Total a pagar ${total}$`)     //POR ULTIMO HACEMOS LA FORMULA PARA SABER EL VALOR EXACTO
 }
